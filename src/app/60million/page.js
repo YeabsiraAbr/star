22ae1acd7aeb6deb60aaf09c97ccfe17e7a93037
Frehiwot702 <frehiwot.tewodros112@gmail.com>
2025-12-29 12:10:26 -0600

    feat/ update 1M draw date

--- src/app/60million/page.js
+++ src/app/60million/page.js
@@ -71,8 +71,8 @@ export default function MillionLotteryPage() {
         },
         body: JSON.stringify({
           amount: 1000000,
-          startDate: "2025-12-15",
-          endDate: "2025-12-21"
+          startDate: "2025-12-22",
+          endDate: "2025-12-28"
         }),
       };
 
